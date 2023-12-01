c0e2185072f8af6ab875112ee31677fe86c481d6
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-12-01 16:24:17 +0300

    Initial Commit

---
+++ CryptoModularizationArchitecture/CryptoModularizationArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoModularizationArchitectureApp.swift
+//  CryptoModularizationArchitecture
+//
+//  Created by Yunus Emre Taşdemir on 1.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoModularizationArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
